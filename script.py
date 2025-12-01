92dfb8ff3f529c95cf6fe0a44323fbf8824cdf95
Aidan Alexander <alexanderaidan12@gmail.com>
2025-11-30 21:28:16 -0500

    artist api call method implementations

--- script.py
+++ script.py
@@ -28,12 +28,62 @@ def get_access_token(client_id, client_secret):
     )
 
     response.raise_for_status()
-    print(response.json())
+    # print(response.json())
     return response.json()["access_token"]
 
+# retrieve artist by artist_id, type string
+def get_artist_name(artist_id, token):
+    url = f"https://api.spotify.com/v1/artists/{artist_id}"
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+    artist = requests.get(
+        url, 
+        headers=headers
+    )
 
+    return artist.json()["name"]
+
+# retrieve genres for an artist by artist_id, type array of strings
+def get_artist_genre(artist_id, token):
+    url = f"https://api.spotify.com/v1/artists/{artist_id}"
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+    artist = requests.get(
+        url, 
+        headers=headers
+    )
+
+    return artist.json()["genre"]
+# retrieve popularity of an artist of scale 0-100, type int
+def get_artist_popularity(artist_id, token):
+    url = f"https://api.spotify.com/v1/artists/{artist_id}"
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+    artist = requests.get(
+        url, 
+        headers=headers
+    )
+# retrieve follower count of an artist, type int
+def get_artist_follow_count(artist_id, token):
+    url = f"https://api.spotify.com/v1/artists/{artist_id}"
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+    artist = requests.get(
+        url, 
+        headers=headers
+    )
+
+    return artist.json()["followers"]["total"]    
+
+
+
+# main
 def main():
     token = get_access_token(CLIENT_ID, CLIENT_SECRET)
-    print(token)
+
 if __name__ == "__main__":
     main()
